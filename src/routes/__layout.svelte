754fdc97cd87c112d110a1e28980068c9ac43c7f
Carlos Ivanchuk <carlosivanchuk@gmail.com>
2021-12-01 13:20:28 -0400

    create 404 not found page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,2 +1,7 @@
 <script>import "../app.css";</script>
-<slot></slot>
+
+<main class="mx-6 mt-5 mb-6 sm:mx-20 sm:mt-10 max-w-7xl xl:mx-auto">
+    <slot></slot>
+
+    <footer class="text-center text-sm font-medium text-montserrat text-gray-3 mt-24">created by <a class="font-bold" rel="noopener noreferrer" target="_blank" href="https://devchallenges.io/portfolio/CarlosIvanchuk">CarlosIvanchuk</a> - <a href="devchallenges.io">devChallenges.io</a></footer>
+</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<h1 class="uppercase font-inconsolata font-bold text-2xl">404 Not Found</h1>
+
+	<div class="mt-16 md:grid md:grid-cols-2 md:gap-x-8 lg:gap-x-28">
+		<figure>
+			<img class="mx-auto md:max-w-lg" alt="drawing of a scarecrow" src="Scarecrow.png">
+		</figure>
+
+		<div class="mt-16 md:mt-0 font-space-mono">
+			<h1 class="text-gray-1 font-bold text-5xl sm:text-6xl">I have bad news for you</h1>
+			<p class="text-gray-2 text-lg sm:text-2xl mt-9">The page that you are looking for might be removed or is temporarily unavailable</p>
+
+			<a class="bg-gray-1 text-white font-bold text-sm py-6 px-11 mt-16 inline-block uppercase" href="/">Back to homepage</a>
+		</div>
+	</div>
+</div>
