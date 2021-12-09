95f492358f5d6e01722f2c073ff23236f623950a
Carlos Ivanchuk <carlosivanchuk@gmail.com>
2021-12-09 05:13:18 -0400

    fix text overlapping

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,12 +5,12 @@
 <div>
 	<h1 class="uppercase font-inconsolata font-bold text-2xl">404 Not Found</h1>
 
-	<div class="mt-16 md:grid md:grid-cols-2 md:gap-x-8 lg:gap-x-28">
-		<figure>
-			<img class="mx-auto md:max-w-lg" alt="drawing of a scarecrow" src="Scarecrow.png">
+	<div class="mt-16 md:grid lg:grid-cols-2 md:gap-x-8 lg:gap-x-28">
+		<figure class="m-auto max-w-md md:max-w-lg">
+			<img alt="drawing of a scarecrow" src="Scarecrow.png">
 		</figure>
 
-		<div class="mt-16 md:mt-0 font-space-mono">
+		<div class="mt-16 lg:mt-0 font-space-mono">
 			<h1 class="text-gray-1 font-bold text-5xl sm:text-6xl">I have bad news for you</h1>
 			<p class="text-gray-2 text-lg sm:text-2xl mt-9">The page that you are looking for might be removed or is temporarily unavailable</p>
 
