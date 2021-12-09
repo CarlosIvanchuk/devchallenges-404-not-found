80ebfe7e7c8af6154ae826a59bc7f5ffff197b87
Carlos Ivanchuk <carlosivanchuk@gmail.com>
2021-12-09 05:12:41 -0400

    change classes order

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script>import "../app.css";</script>
 
-<main class="mx-6 mt-5 mb-6 sm:mx-20 sm:mt-10 max-w-7xl xl:mx-auto">
+<main class="mx-6 mt-5 mb-6 max-w-6xl sm:mx-20 sm:mt-10 xl:mx-auto">
     <slot></slot>
 
     <footer class="text-center text-sm font-medium text-montserrat text-gray-3 mt-24">created by <a class="font-bold" rel="noopener noreferrer" target="_blank" href="https://devchallenges.io/portfolio/CarlosIvanchuk">CarlosIvanchuk</a> - <a href="devchallenges.io">devChallenges.io</a></footer>
